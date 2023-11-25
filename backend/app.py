54abd57af9042a2f2bb114aa83bfecd86d495190
Albert Merino <albertmerino11@gmail.com>
2023-11-25 17:57:39 +0100

    Simple endpoint to recover summary and detail plant info

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,75 @@
-from flask import Flask
+import json
+import sqlite3
 
+from flask import Flask, g, request, Response
+from markupsafe import escape
+
+DATABASE = './database/lleidahack.db'
 app = Flask(__name__)
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        # db.row_factory = sqlite3.Row
+        db.row_factory = dict_factory
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+@app.route("/hello/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}!"
+
+
+@app.route('/plants', methods=['GET'])
+def get_plants_summary():
+    query = f'SELECT distinct thing_id FROM plant_data'
+    conn = get_db()
+    data_points = conn.execute(query).fetchall()
+    conn.close()
+    print(data_points)
+    return Response(json.dumps([plant["thing_id"] for plant in data_points]), mimetype="application/json")
+
+@app.route('/plant/<plant_id>/summary', methods=['GET'])
+def get_plant_summary(plant_id):
+    query = f'SELECT * FROM plant_data where thing_id="{plant_id}" ORDER BY ts DESC LIMIT 1'
+    conn = get_db()
+    data_points = conn.execute(query).fetchall()
+    conn.close()
+    print(data_points)
+    return Response(json.dumps(data_points), mimetype="application/json")
+
+
+@app.route('/plant/<plant_id>/detail', methods=['GET'])
+def get_plant_detail(plant_id):
+    query = f'SELECT * FROM plant_data where thing_id="{plant_id}" ORDER BY ts ASC'
+    if "limit" in request.args.keys():
+        query += f' LIMIT {request.args.get("limit")}'
+    conn = get_db()
+    data_points = conn.execute(query).fetchall()
+    conn.close()
+    return Response(json.dumps(data_points), mimetype="application/json")
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
