71d6b3512277550584c859df8b3ff532c0fec407
Albert Merino <albert.merino@streamsets.com>
2023-11-26 00:00:54 +0100

    Adding prediction to the result

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from flask import jsonify
 
 from flask import Flask, g, request, Response
+from flask import jsonify
 from markupsafe import escape
 
 DATABASE = './database/lleidahack.db'
@@ -35,7 +36,7 @@ def close_connection(exception):
 
 
 @app.route("/hello/<name>")
-def hello(name): 
+def hello(name):
     return f"Hello, {escape(name)}!"
 
 
@@ -47,12 +48,19 @@ def get_plants_summary():
     conn.close()
     return jsonify(data_points)
 
+
 @app.route('/plant/<plant_id>/summary', methods=['GET'])
 def get_plant_summary(plant_id):
     query = f'SELECT * FROM plant_data where thing_id="{plant_id}" ORDER BY ts DESC LIMIT 1'
     conn = get_db()
     data_point = conn.execute(query).fetchone()
     conn.close()
+
+    url = 'http://localhost:5001/prediction/watering'
+    print(data_point)
+    response = requests.post(url, json=json.dumps([data_point]))
+    response = json.loads(response.content)
+    data_point["prediction"] = response[0]
     if data_point:
         return jsonify(data_point)
     else:
